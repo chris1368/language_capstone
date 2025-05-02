da715262f82e1503961fec5ee9d4a65e4dcfc9e9
chris <test>
2025-05-02 15:29:35 +0200

    Add Sec Group

--- main.tf
+++ main.tf
@@ -69,7 +69,35 @@ resource "aws_route_table_association" "public_subnet_asso1" {
   route_table_id = aws_route_table.second_rt.id
 }
 
-resource "aws_route_table_association" "public_subnet_asso2" {
-  gateway_id     = aws_internet_gateway.gw.id
-  route_table_id = aws_route_table.second_rt.id
+# funktioniert nicht 
+#resource "aws_route_table_association" "public_subnet_asso2" {
+#  gateway_id     = aws_internet_gateway.gw.id
+#  route_table_id = aws_route_table.second_rt.id
+#}
+
+# Security Group
+
+resource "aws_security_group" "allow_ssh" {
+  name        = "allow_ssh"
+  description = "Allow ssh inbound traffic"
+  vpc_id      = aws_vpc.wordpress-vpc.id
+
+  ingress {
+    description = "allow SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "allow_ssh"
+  }
 }
