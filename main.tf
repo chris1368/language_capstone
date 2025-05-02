44edd08059c6127df81dc037ad5cf9faecf195cc
chris <test>
2025-05-02 12:00:49 +0200

    Add VPC+Subnet+Internet Gateway+Route Table + Subnet Associating

--- main.tf
+++ main.tf
@@ -46,3 +46,25 @@ resource "aws_internet_gateway" "gw" {
     Name = "internet gateway wordpress"
   }
 }
+
+# Route Table
+
+resource "aws_route_table" "second_rt" {
+ vpc_id = aws_vpc.wordpress-vpc.id
+ 
+ route {
+   cidr_block = "0.0.0.0/0"
+   gateway_id = aws_internet_gateway.gw.id
+ }
+ 
+ tags = {
+   Name = "2nd Route Table"
+ }
+}
+
+# Associate Public Subnets with the Second Route Table
+
+resource "aws_route_table_association" "public_subnet_asso" {
+  gateway_id     = aws_internet_gateway.gw.id
+  route_table_id = aws_route_table.second_rt.id
+}
