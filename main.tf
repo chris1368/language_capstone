bc2f9b03097a56272e6795c5838cf465b0a63bfa
chris <test>
2025-05-09 18:21:14 +0200

    finished5

--- main.tf
+++ main.tf
@@ -193,6 +193,31 @@ resource "aws_security_group" "allow_ssh" {
   }
 }
 
+
+resource "aws_db_subnet_group" "rds_subnet_group" {
+  name       = "rds-subnet-group-new" # <-- change this name
+  subnet_ids = [aws_subnet.private1.id, aws_subnet.private2.id]
+}
+#RDS INSTANCE
+resource "aws_db_instance" "wordpress" {
+  engine                    = "mysql"
+  engine_version            = "5.7"
+  skip_final_snapshot       = true
+  final_snapshot_identifier = "my-final-snapshot"
+  instance_class            = "db.t3.micro"
+  allocated_storage         = 20
+  identifier                = "my-rds-instance"
+  db_name                   = "wordpress_db"
+  username                  = "test"
+  password                  = "test123$"
+  db_subnet_group_name      = aws_db_subnet_group.rds_subnet_group.name
+  vpc_security_group_ids    = [aws_security_group.rds_security_group.id]
+
+  tags = {
+    Name = "RDS Instance"
+  }
+}
+
 data "template_file" "start_userdata" {
   template = <<-EOF
   #!/bin/bash
@@ -249,29 +274,7 @@ data "template_file" "start_userdata" {
 
 }
 
-resource "aws_db_subnet_group" "rds_subnet_group" {
-  name       = "rds-subnet-group-new" # <-- change this name
-  subnet_ids = [aws_subnet.private1.id, aws_subnet.private2.id]
-}
-#RDS INSTANCE
-resource "aws_db_instance" "wordpress" {
-  engine                    = "mysql"
-  engine_version            = "5.7"
-  skip_final_snapshot       = true
-  final_snapshot_identifier = "my-final-snapshot"
-  instance_class            = "db.t3.micro"
-  allocated_storage         = 20
-  identifier                = "my-rds-instance"
-  db_name                   = "wordpress_db"
-  username                  = "test"
-  password                  = "test123$"
-  db_subnet_group_name      = aws_db_subnet_group.rds_subnet_group.name
-  vpc_security_group_ids    = [aws_security_group.rds_security_group.id]
 
-  tags = {
-    Name = "RDS Instance"
-  }
-}
 # RDS security group
 resource "aws_security_group" "rds_security_group" {
   name        = "rds-security-group"
