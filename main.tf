85f0d1fd04aad7a4cba170be8064c736ce0b276b
chris <test>
2025-05-12 13:42:31 +0200

    main_clean2

--- main.tf
+++ main.tf
@@ -6,64 +6,21 @@ terraform {
 
       source  = "hashicorp/aws"
 
-      version = "5.94.1"
+      version = "5.97.0"
 
     }
   }
   }
-data "template_file" "start_userdata" {
-  template = <<-EOF
-  #!/bin/bash
 
-  # Update system packages
-  sudo yum update -y
+data "template_file" "init" {
 
-  # Install Apache (httpd)
-  sudo yum install httpd -y
-  sudo systemctl start httpd
-  sudo systemctl enable httpd
-
-  # Install PHP and required extensions
-  sudo amazon-linux-extras enable php7.4
-  sudo yum clean metadata
-  sudo yum install php php-mysqlnd php-fpm php-xml php-mbstring wget unzip -y
-
-  # Restart Apache
-  sudo systemctl restart httpd
-
-  # Download and extract WordPress
-  wget https://wordpress.org/latest.tar.gz
-  tar -xzf latest.tar.gz
-
-  # Move WordPress files to web directory
-  sudo mv wordpress/* /var/www/html/
-
-  # Set permissions
-  sudo chown -R apache:apache /var/www/html/
-  sudo chmod -R 755 /var/www/html/
-
-  # Configure wp-config.php
-  cd /var/www/html
-  sudo cp wp-config-sample.php wp-config.php
-
-  # Set database credentials
-  sudo sed -i "s/database_name_here/wordpress_db/" wp-config.php
-  sudo sed -i "s/username_here/test/" wp-config.php
-  sudo sed -i "s/password_here/test123$/" wp-config.php
-  sudo sed -i "s/localhost/${rds_endpoint}/" wp-config.php
-
-  sudo chmod 644 wp-config.php
-
-  # Final restart
-  sudo systemctl restart httpd
-
-  echo "WordPress setup with RDS completed!"
-  EOF
+  template = filebase64("userData.sh")
 
   vars = {
-    rds_endpoint = aws_db_instance.wordpress.address
-  }
 
+    rds_endpoint     = aws_db_instance.wordpress.address
+
+  }
 
 }
 
@@ -75,7 +32,7 @@ resource "aws_instance" "wordpress" {
   subnet_id                   = aws_subnet.public1.id
   security_groups             = [aws_security_group.allow_ssh.id]
   associate_public_ip_address = true
-  user_data = base64encode(data.template_file.start_userdata.rendered)
+  user_data = base64encode(data.template_file.init.rendered)
   tags = {
     Name = "WordPress Server 1"
   }
@@ -91,7 +48,7 @@ resource "aws_instance" "wordpress2" {
   subnet_id                   = aws_subnet.public2.id
   security_groups             = [aws_security_group.allow_ssh.id]
   associate_public_ip_address = true
-  user_data = base64encode(data.template_file.start_userdata.rendered)
+  user_data = base64encode(data.template_file.init.rendered)
   tags = {
     Name = "WordPress Server 2"
   }
